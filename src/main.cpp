5c4867ea1c859ff6397733908dfad48d66264969
Antonio A. Carrillo <acarrillo@emuasa.es>
2020-05-24 17:15:52 +0200

    Tested esp32-ble-gamepad example

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,35 @@
 #include <Arduino.h>
 
+#include <BleGamepad.h>
+
+BleGamepad bleGamepad;
 const int ledPin = 2;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
+  Serial.println("Starting BLE work!");
+  bleGamepad.begin();
   pinMode(ledPin, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(ledPin, HIGH);
-  Serial.println("on");
-  delay(500);
-  digitalWrite(ledPin, LOW);
-  delay(500);
-  Serial.println("off");
+void loop()
+{
+  if (bleGamepad.isConnected())
+  {
+    Serial.println("Press buttons 1 and 14. Move all axes to max. Set DPAD to down right.");
+    bleGamepad.press(BUTTON_14);
+    bleGamepad.press(BUTTON_1);
+    bleGamepad.setAxes(127, 127, 127, 127, 127, 127, DPAD_DOWN_RIGHT);
+    delay(500);
+    digitalWrite(ledPin, HIGH);
+    Serial.println("on");
+
+    Serial.println("Release button 14. Move all axes to min. Set DPAD to centred.");
+    bleGamepad.release(BUTTON_14);
+    bleGamepad.setAxes(-127, -127, -127, -127, -127, -127, DPAD_CENTERED);
+    delay(500);
+    digitalWrite(ledPin, LOW);
+    Serial.println("off");
+  }
 }
